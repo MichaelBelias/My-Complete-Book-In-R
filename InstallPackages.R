2a6d744f3afe15674cd1ed86954a51a6e1af4d32
Fideborios <touringlxs@gmail.com>
2017-06-19 11:26:33 +0200

    with Detach

--- InstallPackages.R
+++ InstallPackages.R
@@ -9,15 +9,15 @@ if(!is_installed("devtools"))
 library("devtools",character.only=TRUE,quietly=TRUE,verbose=FALSE) 
 
 
-package_names= c("hglm","foreign","knitr","ggplot2","ggthemes","ggThemeAssist","GGally","reshape2",
+package_names= c("hglm","foreign","knitr","caret","rstan","rstanarm","ggplot2","ggthemes","ggThemeAssist","GGally","reshape2",
                  "lme4","nlme","compiler","metafor","sas7bdat","memisc","parallel",
                  "boot","nlme", "MASS","lme4","Rglpk","mvmeta","netmeta",
                  "gemtc", "msm","Formula","gems", "Epi", "simMSM","pander",
                  "gemtc","coda", "ipdmeta","R2jags","ape","knitr","ggmcmc",
-                 "RCurl","XML","ips","RMySQL","svDialogs","Rphylip","rdrop2",
+                 "RCurl","XML","ips","RMySQL","svDialogs","Rphylip",
                  "xlsx","devtools","httpuv","png","grid","phytools","phangorn",
-                 "rstan","rstanarm","car","survival","corrplot","COUNT",
-                 "knitr","diagram","stringi","metafor","caret")
+                 "car","survival","corrplot","COUNT",
+                 "knitr","diagram","stringi","metafor","rdrop2")
 
 
 
@@ -35,5 +35,19 @@ for(package_name in package_names)
   
 }  
 
-rm(package_name,package_names,is_installed)
+detachAllPackages <- function() {
+  
+  basic.packages <- c("package:stats","package:graphics","package:grDevices","package:utils","package:datasets","package:methods","package:base")
+  
+  package.list <- search()[ifelse(unlist(gregexpr("package:",search()))==1,TRUE,FALSE)]
+  
+  package.list <- setdiff(package.list,basic.packages)
+  
+  if (length(package.list)>0)  for (package in package.list) detach(package, character.only=TRUE)
+  
+}
+
+detachAllPackages()
+
+rm(package_name,package_names,is_installed,detachAllPackages)
 search()
